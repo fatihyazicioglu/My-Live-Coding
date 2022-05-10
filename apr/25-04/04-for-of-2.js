71b149dd92fd7cb7a74ef3e921416d4b184913f5
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-05-10 23:30:39 +0200

    updated

--- apr/25-04/04-for-of-2.js
+++ apr/25-04/04-for-of-2.js
@@ -21,35 +21,59 @@ const students = [
   },
 ];
 
-for (let eachStudent of students) {
-  console.log(eachStudent);
+for (let xyz of students) {
+  console.log(xyz);
 }
 
-// show only students that are older than 25
-console.log("\n\nStudents older than 25:\n");
-for (let eachStudent of students) {
-  if (eachStudent.age > 25) {
-    console.log(eachStudent);
-  }
+// // show only students that are older than 25
+// console.log("\n\nStudents older than 25:\n");
+//  for (let eachStudent of students) {
+//    if (eachStudent.age > 25) {
+//      console.log(eachStudent);
+//    }
+//  }
+
+ console.log("\n\nStudents older than 25:\n");
+
+for (let x of students){
+if (x.age>25){
+  console.log(x);
+}
 }
 
-// only students with an average above 7
+// // only students with an average above 7
+// console.log("\n\nStudents with average above 7:\n");
+
+// for (let eachStudent of students) {
+//   if (eachStudent.averageResults > 7) {
+//     console.log(eachStudent);
+//   }
+// }
 console.log("\n\nStudents with average above 7:\n");
 
-for (let eachStudent of students) {
-  if (eachStudent.averageResults > 7) {
-    console.log(eachStudent);
+for(let student of students){
+  if(student.averageResults>7){
+    console.log(student);
   }
+
 }
 
-// students whose name starts with letter "m"
+// // students whose name starts with letter "m"
+
+// console.log("\n\nStudents with names that start with letter m:\n");
+// for (let eachStudent of students) {
+//   if (eachStudent.name.toLowerCase().startsWith("m")) {
+//     console.log(eachStudent);
+//   }
+// }
 
 console.log("\n\nStudents with names that start with letter m:\n");
-for (let eachStudent of students) {
-  if (eachStudent.name.toLowerCase().startsWith("m")) {
-    console.log(eachStudent);
+for (let student of students){
+  if (student.name.toLowerCase().startsWith("m")){
+    console.log(student);
   }
 }
+console.log("\n\n123\n");
 
 function studentsByAverage(array, average) {
   const result = [];
