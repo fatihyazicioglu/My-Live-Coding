1c16b232a6947fa1927c42a66a33cf7cfdbe89cd
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-05-11 23:13:00 +0200

    updated

--- apr/26-04/02-merge-objects.js
+++ apr/26-04/02-merge-objects.js
@@ -13,16 +13,18 @@ let address = {
   number: 21,
   city: "Berlin",
 };
-
+let merged = Object.assign(person, address)
 // let's merge the 2 objects above into a new one
 
 let idCard = Object.assign(person, address);
 
-console.log(idCard);
+console.log(idCard, merged);
 
 let x = { a: 1, b: 2 };
-let y = { a: 99, c: 3 };
-
+let y = { a: 99, c: 3 , b:3};
+ 
+let mergedOne = Object.assign(x,y)
 let z = Object.assign(y, x);
 
 console.log(z);
+console.log(mergedOne);
