c7519ae192b04074c0b8dc4fe49725a74de91eaa
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-05-17 10:06:49 +0200

    updated

--- may/03-05/01-callback.js
+++ may/03-05/01-callback.js
@@ -17,32 +17,32 @@ function sum(a, b) {
 let result = applyFunction(2, 3, sum);
 console.log(result);
 
-let result2 = applyFunction(2, 3, (a, b) => a + b);
-console.log(result2);
+ let result2 = applyFunction(2, 3, (a, b) => a + b);
+ console.log(result2);
 
-let result3 = applyFunction(2, 3, (a, b) => a * b);
-let result4 = applyFunction(2, 3, (a, b) => a / b);
-let result5 = applyFunction(2, 3, (a, b) => a < b);
+ let result3 = applyFunction(2, 3, (a, b) => a * b);
+ let result4 = applyFunction(2, 3, (a, b) => a / b);
+ let result5 = applyFunction(2, 3, (a, b) => a < b);
 
-let result6 = applyFunction(2, 3, function (a, b) {
-  return a < b;
-});
+ let result6 = applyFunction(2, 3, function (a, b) {
+   return a < b;
+ });
 
-// function double(n) {
-//     let result = n * 2;
-//     return result;
-// }
+ function double(n) {
+     let result = n * 2;
+     return result;
+ }
 
-// function triple(n) {
-//     let result = n * 3;
-//     return result;
-// }
+ function triple(n) {
+    let result = n * 3;
+     return result;
+ }
 
-// function square(n) {
-//     let result = n ** 2;
-//     return result;
-// }
+ function square(n) {
+     let result = n ** 2;
+     return result;
+ }
 
-// function applyFunction(n, fn) {
-//     let result = fn(n);
-// }
+//  function applyFunction(n, fn) {
+//      let result = fn(n);
+//  }
