6a84e7b2a837ea92a504b6ec65ec591e84a2c841
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-05-18 22:25:01 +0200

    updated

--- may/05-05/01-reduce.js
+++ may/05-05/01-reduce.js
@@ -1,23 +1,23 @@
 let numbers = [1, 2, 3, 4, 5];
 let total = 0;
 
-for (let i = 0; i < numbers.length; i++) {
-  console.log("previous value", total);
-  console.log("current value", numbers[i]);
-  total = total + numbers[i];
-  console.log("new value", total);
-  console.log("-------------");
-}
-console.log(total);
-
-// function sum(previousValue, currentValue) {
-//   console.log("previousValue", previousValue);
-//   console.log("currentValue", currentValue);
-//   let newValue = previousValue + currentValue;
-//   console.log("new value", newValue);
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log("previous value", total);
+//   console.log("current value", numbers[i]);
+//   total = total + numbers[i];
+//   console.log("new value", total);
 //   console.log("-------------");
-//   return newValue;
 // }
+// console.log(total);
+
+function sum(previousValue, currentValue) {
+  console.log("previousValue", previousValue);
+  console.log("currentValue", currentValue);
+  let newValue = previousValue + currentValue;
+  console.log("new value", newValue);
+  console.log("-------------");
+  return newValue;
+}
 
 
 function sum(previousValue, currentValue) {
@@ -26,27 +26,27 @@ function sum(previousValue, currentValue) {
 
 let totalReduce = numbers.reduce(sum, 0);
 
-let totalReduce2 = numbers.reduce((a, b) => a + b , 0);
-console.log("totalReduce", totalReduce);
-console.log("totalReduce2", totalReduce2);
+ let totalReduce2 = numbers.reduce((a, b) => a + b , 0);
+ console.log("totalReduce", totalReduce);
+ console.log("totalReduce2", totalReduce2);
 
 
-const string = "hello world";
+// const string = "hello world";
 
-// use reduce() to get a string with only letters in even positions:
-// -> hlowrd
+// // use reduce() to get a string with only letters in even positions:
+// // -> hlowrd
 
-let stringArray = [...string];
-console.log(stringArray);
+// let stringArray = [...string];
+// console.log(stringArray);
 
-function onlyEvenLetters(previousValue, currentValue, index) {
-    if(index % 2 === 0) {
-        // let's add the current value to the previous one
-        return previousValue + currentValue;
-    } else {
-        // we need to keep the previous value as it is, let's just return it
-        return previousValue;
-    } 
-}
-const evenLetters = stringArray.reduce(onlyEvenLetters, "");
-console.log(evenLetters);
+// function onlyEvenLetters(previousValue, currentValue, index) {
+//     if(index % 2 === 0) {
+//         // let's add the current value to the previous one
+//         return previousValue + currentValue;
+//     } else {
+//         // we need to keep the previous value as it is, let's just return it
+//         return previousValue;
+//     } 
+// }
+// const evenLetters = stringArray.reduce(onlyEvenLetters, "");
+// console.log(evenLetters);
