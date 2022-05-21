861600a90de7aeaacbbe99dcc0407de897342ea0
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-05-21 11:12:13 +0200

    updated

--- may/09-05/04-sort-catch.js
+++ may/09-05/04-sort-catch.js
@@ -2,7 +2,11 @@ let numbers = [1, 2, 8, 3, 0, -2];
 
 let numbersCopy = [...numbers];  
 
-let numbersSorted = [...numbers].sort((a, b) => a - b);
+let numbersSorted = [...numbers].sort((a, b) => b - a);
+let numbersSorted2 = [...numbers].sort((a, b) => a- b);
+
 
 console.log("sorted", numbersSorted)
 console.log("original", numbers)
+
+console.log("sorted", numbersSorted2)
