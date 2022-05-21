efd92add6633747bc9d1f29a9e1323940f3628ac
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-05-21 22:09:27 +0200

    updated

--- may/10-05/class.js
+++ may/10-05/class.js
@@ -9,18 +9,17 @@ class Rectangle{
         return 2 * width+ height
     }
     area() {
-        return width * this.height
+        return  this.width * this.height
     }
     description(){
         return `this is a ${this.color} rectangle ${this.width}cm width and ${this.height}cm height`
     }
 }
 
-let rectangle = new Rectangle(4,3, "black")
-let rectangle1= new Rectangle (2, 6, "blue")
+ let rectangle = new Rectangle(4,3, "black")
+ let rectangle1= new Rectangle (2, 6, "blue")
 
-console.log(rectangle)
-console.log(rectangle.area())
-console.log(rectangle.description())
-console.log(rectangle1)
-console.log(rectangle1.description())
+ console.log(rectangle)
+ console.log(rectangle1.perimeter);
+ console.log(rectangle1)
+ console.log(rectangle1.description())
